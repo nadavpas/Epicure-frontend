9c3368488b96fec39a6d500823406f953b67ae2b
nadavpas <nadavp@moveo.co.il>
2025-06-15 16:45:56 +0300

    the header looks like in the figma. need to understand figma more

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,21 +6,22 @@ import "../styles/Header.scss";
 function Header() {
   return (
     <header>
-      <div className="left-section">
-        <div className="logo-section">
-          <img src={logo} alt="logo-icon" />
-          <span className="logo-text">EPICURE</span>
+        <div className="left-section">
+          <div className="logo-section">
+            <img src={logo} alt="logo-icon" />
+            <span className="logo-text">EPICURE</span>
+          </div>
+          <nav>
+            <button>Restaurants</button>
+            <button>Chefs</button>
+          </nav>
         </div>
-        <nav>
-          <button>Restaurants</button>
-          <button>Chefs</button>
-        </nav>
-      </div>
-      <div className="right-section">
-        <img src={search} alt="search-icon" />
-        <img src={user} alt="user-icon" />
-        <img src={bag} alt="bag-icon" />
-      </div>
+        <div className="right-section">
+          <img src={search} alt="search-icon" />
+          <img src={user} alt="user-icon" />
+          <img src={bag} alt="bag-icon" />
+        </div>
+    
     </header>
   );
 }
