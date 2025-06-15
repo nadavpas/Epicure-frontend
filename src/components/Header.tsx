ccc5265959fdca04bc2b02d3b28dc4a14c4424cb
nadavpas <nadavp@moveo.co.il>
2025-06-15 14:52:43 +0300

    more styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,25 @@
-import logo from "../assets/logo.svg"
-import bag from "../assets/bag.svg"
-import search from "../assets/search.svg"
-import user from "../assets/user.svg"
+import logo from "../assets/logo.svg";
+import bag from "../assets/bag.svg";
+import search from "../assets/search.svg";
+import user from "../assets/user.svg";
+import "../styles/Header.scss";
 function Header() {
   return (
-    <header
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      <div
-        className="logo-section"
-        style={{ display: "flex", alignItems: "center" }}
-      >
-        <img
-          src={ logo }
-          alt="logo-icon"
-        />
-        <span className="logo-text">EPICURE</span>
+    <header>
+      <div className="left-section">
+        <div className="logo-section">
+          <img src={logo} alt="logo-icon" />
+          <span className="logo-text">EPICURE</span>
+        </div>
+        <nav>
+          <button>Restaurants</button>
+          <button>Chefs</button>
+        </nav>
       </div>
-      <nav>
-        <button>Restaurants</button>
-        <button>Chefs</button>
-      </nav>
       <div className="right-section">
-        <img src={ bag } alt="bag-icon" />
-        <img src={ user } alt="user-icon" />
-        <img src={ search } alt="search-icon" />
+        <img src={search} alt="search-icon" />
+        <img src={user} alt="user-icon" />
+        <img src={bag} alt="bag-icon" />
       </div>
     </header>
   );
