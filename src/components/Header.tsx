59f724090dcc5a89515eeef8c3eb8ca1eaaed4cf
nadavpas <nadavp@moveo.co.il>
2025-06-16 11:15:57 +0300

    toggle search box

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,31 +6,34 @@ import { useState } from "react";
 import "../styles/Header.scss";
 
 function Header() {
-   const [showSearch,setShowSearch] = useState(false);
-    const toggleSearch = ()=>{
-        setShowSearch((prev) => !prev);
-    }
+  const [showSearch, setShowSearch] = useState(false);
+  const toggleSearch = () => {
+    setShowSearch((prev) => !prev);
+  };
   return (
     <header>
-        <div className="left-section">
-          <div className="logo-section">
-            <img src={logo} alt="logo-icon" />
-            <span className="logo-text">EPICURE</span>
-          </div>
-          <nav>
-            <button>Restaurants</button>
-            <button>Chefs</button>
-          </nav>
+      <div className="left-section">
+        <div className="logo-section">
+          <img src={logo} alt="logo-icon" />
+          <span className="logo-text">EPICURE</span>
         </div>
-        <div className="right-section">
-            {showSearch && (
-                <input type="text" placeholder="|Search for restaurant, cuisine, chef" />
-            )}
-          <img src={search} alt="search-icon" onClick={toggleSearch}/>
-          <img src={user} alt="user-icon" />
-          <img src={bag} alt="bag-icon" />
-        </div>
-    
+        <nav>
+          <button>Restaurants</button>
+          <button>Chefs</button>
+        </nav>
+      </div>
+      <div className="right-section">
+        {showSearch && (
+          <input
+            className="search-box"
+            type="text"
+            placeholder="|Search for restaurant, cuisine, chef"
+          />
+        )}
+        <img src={search} alt="search-icon" onClick={toggleSearch} />
+        <img src={user} alt="user-icon" />
+        <img src={bag} alt="bag-icon" />
+      </div>
     </header>
   );
 }
