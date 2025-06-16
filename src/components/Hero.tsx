738db5d1bd2ec11688e8b5777e7d0615a7dedf00
nadavpas <nadavp@moveo.co.il>
2025-06-16 15:01:35 +0300

    added the background picture

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,18 @@
-import search from '../assets/search.svg'
-import '../styles/Hero.scss'
-function Hero(){
-    return(
-        <div className="container">
-            <h3 className='text'> Epicure works with the top chef restaurants in Tel Aviv</h3>
-            <div className="search-section" >
-                <img src={search} alt="" />
-                <input type="text" placeholder='Search for restaurant cuisine,chef' />
-            </div>
+import search from "../assets/search.svg";
+import "../styles/Hero.scss";
+function Hero() {
+  return (
+    <div className="container">
+      <div className="hero-container">
+        <h3 className="text">
+          Epicure works with the top chef restaurants in Tel Aviv
+        </h3>
+        <div className="search-section">
+          <img src={search} alt="" />
+          <input type="text" placeholder="Search for restaurant cuisine,chef" />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-export default Hero
+export default Hero;
