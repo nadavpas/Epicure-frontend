eebc486d913f64ff430e239231a6a0876224e9e3
nadavpas <nadavp@moveo.co.il>
2025-06-17 14:15:24 +0300

    turned the container to a flex box

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -1,20 +1,20 @@
-import claro from '../assets/claro.svg';
-import '../styles/RestaurantCard.scss';
-interface RestaurantProps{
-  chef: string,
-  restaurant: string,
+import claro from "../assets/claro.svg";
+import "../styles/RestaurantCard.scss";
+interface RestaurantProps {
+  chef: string;
+  restaurant: string;
   // image: string
 }
-function RestaurantCard({chef, restaurant}:RestaurantProps) {
-  return <div className="container">
-    <img src={ claro } alt="dish" />
-    <div className='info'>
-      <h3>{restaurant}</h3>
-      <h4>{chef}</h4>
-      <h5> 
-          Rating
-      </h5>
+function RestaurantCard({ chef, restaurant }: RestaurantProps) {
+  return (
+    <div className="container">
+      <img src={claro} alt="dish" />
+      <div className="info">
+        <h3>{restaurant}</h3>
+        <h4>{chef}</h4>
+        <h5>Rating</h5>
+      </div>
     </div>
-  </div>;
+  );
 }
 export default RestaurantCard;
