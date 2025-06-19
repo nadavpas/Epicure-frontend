4b193b8fa40ad07a9a4745e6186b7cf236c0d861
nadavpas <nadavp@moveo.co.il>
2025-06-19 12:45:26 +0300

    turned rating to a prop

--- src/components/Popular.tsx
+++ src/components/Popular.tsx
@@ -1,10 +1,11 @@
 import RestaurantCard from "./RestaurantCard";
 import "../styles/Populer.scss";
+import Rating from "./Rating";
 function Popular() {
   const cards_arr = [
-    { restaurant: "Claro", chef: " Ran Shmueli" },
-    { restaurant: "Capara Mio", chef: "Nadav" },
-    { restaurant: "M25", chef: "Sapir" },
+    { restaurant: "Claro", chef: " Ran Shmueli",rating:5 },
+    { restaurant: "Capara Mio", chef: "Nadav",rating:3 },
+    { restaurant: "M25", chef: "Sapir",rating:4 },
   ];
   return (
     <div className="popular-restaurants">
@@ -16,6 +17,7 @@ function Popular() {
                 key={card.restaurant}
                 restaurant={card.restaurant}
                 chef={card.chef}
+                rating={card.rating}
               />
           );
         })}
--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -5,16 +5,17 @@ interface RestaurantProps {
   chef: string;
   restaurant: string;
   key: string;
+  rating:number
   // image: string
 }
-function RestaurantCard({ chef, restaurant }: RestaurantProps) {
+function RestaurantCard({ chef, restaurant,rating }: RestaurantProps) {
   return (
     <div className="container">
       <img src={claro} alt="dish" />
       <div className="info">
         <h3>{restaurant}</h3>
         <h4>{chef}</h4>
-        <Rating rating={3}/>
+        <Rating rating={rating}/>
       </div>
     </div>
   );
