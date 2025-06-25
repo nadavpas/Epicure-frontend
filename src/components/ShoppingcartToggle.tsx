69430c85670c0b2995d81b1fe470b32cba4d3b8d
nadavpas <nadavp@moveo.co.il>
2025-06-25 17:51:55 +0300

    moved all of Header's components into the header file as functions

--- src/components/ShoppingcartToggle.tsx
+++
@@ -1,5 +0,0 @@
-import { bagIcon } from "../assets/Icons";
-const ShoppingcartToggle: React.FC = () => {
-  return <img src={bagIcon} alt="bag-icon" />;
-};
-export default ShoppingcartToggle;
--- src/components/UserToggle.tsx
+++
@@ -1,5 +0,0 @@
-import { userIcon } from "../assets/Icons";
-const UserToggle: React.FC = () => {
-  return <img src={userIcon} alt="user-icon" />;
-};
-export default UserToggle;
