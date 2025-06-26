51a8be43418ec46be7cdd006c00d3f031c44499d
nadavpas <nadavp@moveo.co.il>
2025-06-26 12:49:19 +0300

    displays three cards

--- src/components/Signature-Dish/SignatureDish.tsx
+++ src/components/Signature-Dish/SignatureDish.tsx
@@ -1,16 +1,17 @@
-import { dishes } from "../../mockData/Dishes"
-import '../../App.scss'
-import DishCard from "../Dishes/DishCard"
-const SignatureDish:React.FC = () => {
-    return(
-        <div className="signature-container">
-            <h2>SIGNATURE DISH OF:</h2>\
-            <div className="dishCard-container">
-                
-            </div>
-
-            <DishCard {...dishes[0]}/>
-        </div>
-    )
-}
-export default SignatureDish
+import { dishes } from "../../mockData/Dishes";
+import '../../styles/SignatureDish.scss'
+import "../../App.scss";
+import DishCard from "../Dishes/DishCard";
+const SignatureDish: React.FC = () => {
+  return (
+    <div className="signature-container">
+      <h2>SIGNATURE DISH OF:</h2>
+      <div className="dishCard-container">
+        {dishes.map((dish) => {
+          return <DishCard {...dish} />;
+        })}
+      </div>
+    </div>
+  );
+};
+export default SignatureDish;
