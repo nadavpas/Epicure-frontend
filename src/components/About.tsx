a23e1d6d1dfe552cd0380149295276809dbc580d
nadavpas <nadavp@moveo.co.il>
2025-06-26 10:35:13 +0300

    aligned all text to the left

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-import { about,googlePlay,appStore } from "../assets/icons";
+import { about,googlePlay,appStore } from "../assets/Icons";
 import '../styles/About.scss'
 function About() {
   return (
