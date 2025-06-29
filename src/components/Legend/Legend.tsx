20507f76bb943069c894e608e363b2abdae50b28
nadavpas <nadavp@moveo.co.il>
2025-06-29 12:45:57 +0300

    used the enum

--- src/components/Legend/Legend.tsx
+++ src/components/Legend/Legend.tsx
@@ -1,13 +1,13 @@
 import "../../styles/Legend.scss";
+import { ExtraType } from "../../utils/enum";
 import IconCard from "./IconCard";
 const Legend: React.FC = () => {
-  const icons = ["spicy", "vegan", "vegetarian"];
   return (
     <div className="legend-container">
       <h2>THE MEANING OF OUR ICONS:</h2>
       <div className="iconCards">
-        {icons.map((icon,i) => {
-          return <IconCard name={icon as "spicy" | "vegan" | "vegetarian"} key={i} />;
+        {Object.values(ExtraType).map((icon,i) => {
+          return <IconCard name={icon} key={i} />;
         })}
       </div>
     </div>
