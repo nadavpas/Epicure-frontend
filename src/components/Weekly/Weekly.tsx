0f3f1918fbf296e08a002d22bf45b26a5428659e
nadavpas <nadavp@moveo.co.il>
2025-06-29 13:01:59 +0300

    broke the weekly component into multiple components

--- src/components/Weekly/Weekly.tsx
+++ src/components/Weekly/Weekly.tsx
@@ -1,30 +1,46 @@
 import ChefCard from "./ChefCard";
 import "../../styles/Weekly.scss";
-import '../../App.scss'
+import "../../App.scss";
 import ChefRestCard from "./ChefRestCard";
 import { restaurants } from "../../mockData/Restaurants";
-import {ChefNames} from "../../utils/enum";
+import { ChefNames } from "../../utils/enum";
 function Weekly() {
+  const RenderArticleText = () => {
+    return (
+      <span className="article-text">
+        Chef Yossi Shitrit has been living and breathing his culinary dreams for
+        more than two decades, including running the kitchen in his first
+        restaurant, the fondly-remembered Violet, located in Moshav Udim.
+        Shitrit's creativity and culinary acumen born of long experience are
+        expressed in the every detail of each and every dish.
+      </span>
+    );
+  };
+  const RenderChefsRestaurants = () => {
+    return (
+      <div className="weekly-rest-cards">
+        {restaurants.map((restaurant) => {
+          return (
+            <ChefRestCard
+              name={restaurant.restaurant}
+              image={restaurant.image}
+              key={restaurant.restaurant}
+            />
+          );
+        })}
+      </div>
+    );
+  };
   return (
     <div className="weekly-container">
       <h3 className="h3">CHEF OF THE WEEK:</h3>
       <div className="weekly-chef">
         <div className="chef-info">
           <ChefCard name={ChefNames.EYALSHANI} />
-          <span className="article-text">
-            Chef Yossi Shitrit has been living and breathing his culinary dreams
-            for more than two decades, including running the kitchen in his
-            first restaurant, the fondly-remembered Violet, located in Moshav
-            Udim. Shitrit's creativity and culinary acumen born of long
-            experience are expressed in the every detail of each and every dish.
-          </span>
+          {RenderArticleText()}
         </div>
         <h3 className="h3">Yossi's Restaurants</h3>
-        <div className="weekly-rest-cards">
-          {restaurants.map((restaurant) => {
-            return <ChefRestCard name={restaurant.restaurant} image={restaurant.image} key={restaurant.restaurant}/>;
-          })}
-        </div>
+        {RenderChefsRestaurants()}
       </div>
     </div>
   );
