74c702632c7f19a753307ea60a6d62c3c7178880
nadavpas <nadavp@moveo.co.il>
2025-06-29 10:08:38 +0300

    resolved all merge conflicts

--- src/assets/Icons/index.ts
+++ src/assets/Icons/index.ts
@@ -1,12 +1,13 @@
-import spicy from './spicy.svg';
-import vegan from './vegan.svg';
-import vegetarian from './vegetarian.svg'
-import ils from './ils.svg';
-import lineIcon from './Line.svg';
 import bagIcon from './bag.svg';
-import logoIcon from './logo.svg';
 import searchIcon from './search.svg';
-import userIcon from './user.svg'
+import userIcon from './user.svg';
+import logoIcon from './logo.svg';
 import more from './more.svg'
+import ils from './ils.svg'
+import lineIcon from './Line.svg'
+import spicy from './spicy.svg'
+import vegan from './vegan.svg'
+import vegetarian from './vegetarian.svg'
 
-export {spicy,vegan,vegetarian,lineIcon,ils,bagIcon,logoIcon,searchIcon,userIcon,more};
+
+export {bagIcon,userIcon,searchIcon,logoIcon,more,ils,lineIcon, spicy, vegan, vegetarian }
--- src/assets/icons/index.ts
+++
@@ -1,7 +0,0 @@
-import bagIcon from './bag.svg';
-import searchIcon from './search.svg';
-import userIcon from './user.svg';
-import logoIcon from './logo.svg';
-import dish from './dish.png'
-
-export {bagIcon,userIcon,searchIcon,logoIcon,dish}
