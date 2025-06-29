78af0974ef8e33d48f4a9b4c7199b2082d820003
nadavpas <nadavp@moveo.co.il>
2025-06-29 13:08:34 +0300

    broke about into multiple components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,31 @@
 import { about,googlePlay,appStore } from "../assets/icons";
 import '../styles/About.scss'   
 function About() {
-  return (
-    <div className="about-container">
-      <div className="about-info">
-        <h3>ABOUT US:</h3>
-        <span className="article-text">
+  const RenderText = () => {
+    return(
+      <span className="article-text">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
           reiciendis natus provident tempore, obcaecati, quae ab eius
           praesentium, amet libero nam ipsa expedita veritatis. Obcaecati labore
           nesciunt laborum eaque accusamus!
         </span>
-        <div className="logos">
+    )
+  };
+  const RenderLogos =() =>{
+    return(
+<div className="logos">
             <img src={googlePlay} alt="googlePlay" />
             <img src={appStore} alt="appStore" />
 
         </div>
+    )
+  }
+  return (
+    <div className="about-container">
+      <div className="about-info">
+        <h3>ABOUT US:</h3>
+        {RenderText()}
+        {RenderLogos()}
       </div>
       <div className="about-logo">
         <img src={about} alt="about-logo" />
