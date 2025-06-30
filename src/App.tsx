d3c5cd8dad2ce9aa7c63ab98b4b3d6b0a0257bae
nadavpas <nadavp@moveo.co.il>
2025-06-30 12:27:28 +0300

    first design

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { useState } from "react";
 import "./App.scss";
-import About from "./components/About/About";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import Legend from "./components/Legend/Legend";
-import Popular from "./components/PopularRestaurants/Popular";
-import SignatureDish from "./components/Signature-Dish/SignatureDish";
-import Weekly from "./components/Weekly/Weekly";
 import SignInForm from "./components/SignIn/SignInForm";
 
 function App() {
-  const [showCart, setShowCart] = useState(false);
-  const toggleShowCart = () => {
-    setShowCart((prev) => !prev);
-  };
   return (
     <div className="app">
       <SignInForm />
--- src/components/SignIn/SignInForm.tsx
+++ src/components/SignIn/SignInForm.tsx
@@ -1,10 +1,41 @@
-const SignInForm:React.FC = () => {
-    <dialog>
+import { lineIcon } from "../../assets/Icons";
+import "../../styles/SignInForm.scss";
+import "../../App.scss";
+const SignInForm: React.FC = () => {
+  const RenderInputs = () => {
+    return (
+      <div className="input-container">
+        <input type="email" placeholder="Email adress" />
+        <input type="password" placeholder="Password" />
+      </div>
+    );
+  };
+  const RenderOr = () => {
+    return (
+      <div>
+        <img src={lineIcon} alt="line" />
+        <span>or</span>
+        <img src={lineIcon} alt="line" />
+      </div>
+    );
+  };
+  return (
+    <dialog open>
+      <div className="signin-container">
         <form method="dialog">
-            <h2>SIGN IN</h2>
-            <label htmlFor="email"> email address</label>
-            <input type="email" name="email"/>
+          <button>Close</button>
+          <div className="signin-headers">
+            <span className="h1">SIGN IN</span>
+            <span className="h4">To continue the order, please sign in</span>
+          </div>
+          {RenderInputs()}
+          <button className="button-text">LOGIN</button>
+          <button>Forget password?</button>
+          {RenderOr()}
         </form>
+        <button className="button-text">SIGN IN</button>
+      </div>
     </dialog>
-}
-export default SignInForm
+  );
+};
+export default SignInForm;
